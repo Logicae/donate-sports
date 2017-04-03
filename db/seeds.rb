50f882b5e18ad723f2566f1d3d4477f26d9db55e
Logicae <logicae@users.noreply.github.com>
2017-04-03 14:05:27 -0400

    started to build out user show page and added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Sport.create(name: "Baseball")
+User.create(email: "bob@bob.com", name: "Bob")
+Product.create(product_name: "Bat", product_description: "Barely used bat")
