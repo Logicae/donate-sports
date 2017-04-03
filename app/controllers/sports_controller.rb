3a2590973b3d8b9c991d7f51a719b1953617354d
Logicae <logicae@users.noreply.github.com>
2017-04-03 14:19:33 -0400

    added sports_params and new actions, and started sports new form

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -4,10 +4,21 @@ class SportsController < ApplicationController
         @sports = Sport.all 
     end 
     
+    def new
+        @sport = Sport.new 
+    end 
+    
     def show
     end 
 
     def create
+
+    end 
+    
+    private 
+
+    def sports_params 
+        params.require(:sport).permit(:name)
     end 
     
 end
