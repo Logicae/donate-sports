4144746d7fa7b22df92bbf90d2becdc62cef2151
Logicae <logicae@users.noreply.github.com>
2017-04-05 14:33:37 -0400

    added action and link for facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sports
 
+  get '/auth/facebook/callback' => 'sessions#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
