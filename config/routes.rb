cc6effadda451060cee04b28a6e942c54fb4e629
Logicae <logicae@users.noreply.github.com>
2017-04-06 19:25:45 -0400

    made changes to omniauth details to attempt problem fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sports
 
-  get 'auth/facebook/callback', to: 'sessions#create'
+  #get 'auth/facebook/callback', to: 'sessions#create'
   get 'auth/failure', to: 'users#show'
 
   get '/login', to: 'sessions#new'
