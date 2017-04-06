351c9283f0b7561d583a44516a93dc951448049b
Logicae <logicae@users.noreply.github.com>
2017-04-06 17:38:33 -0400

    added route, link, and config for oauth facebook login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,24 @@ class SessionsController < ApplicationController
     end
 
     def create
+        if request.env['omniauth.auth']
+            @user = User.create_with_omniauth(request.env['omniauth.auth'])
+            session[:user_id] = @user.id    
+            redirect_to user_path(@user)
+        else
+            @user = User.find_by(email: params[:user][:email])
+            if @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
+            else 
+                redirect_to login_path
+            end  
+        end
+    end
+    
+    def auth
+        binding.pry
+        request.env['omniauth.auth']
     end
 
     def destroy
