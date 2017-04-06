45c7388018fd774c52b26121debd555159f9e4b8
Logicae <logicae@users.noreply.github.com>
2017-04-06 09:26:58 -0400

    re-added oauth route and added method to user class for oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
     validates :password, presence: true
 
     has_secure_password
+
+    def self.find_or_create_by_omniauth(auth_hash)
+        self.where(:email => auth_hash["omniauth.auth"]["info"]["email"]).first_or_create do |user|
+            user.password = SecureRandom.hex
+        end
+    end
 end
