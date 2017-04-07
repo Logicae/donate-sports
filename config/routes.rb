3e72a3af2b8f71f23f21376e14af20a98c542c64
Logicae <logicae@users.noreply.github.com>
2017-04-07 09:58:35 -0400

    added nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   get 'auth/github', :as => 'github_auth'
   match 'auth/github/callback', to: 'sessions#create', :via => [:get, :post]
 
+resources :users, only: [:show] do
+    # nested resource for products
+    resources :products, only: [:show]
+  end
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
