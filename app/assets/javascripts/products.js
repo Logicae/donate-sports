fc808c29c46a53d72ee570214069b41b1df7f177
Logicae <logicae@users.noreply.github.com>
2017-05-19 16:08:40 -0400

    added js file in pipeline and required it

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  attachListeners();
+})
+
+function attachListeners() {
+  $("#products").click(function() {
+      alert("hello");
+  })
+}
+//      $("#message").text(hello)})
+
+
+//function hello() {
+//    var hello = "hello"
+//    $("#message").text(hello);
+//}
