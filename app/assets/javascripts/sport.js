030eb9662bbd1321c3e8a7a2a5bef39e22aa5936
github username <logicae@users.noreply.github.com>
2017-05-23 16:41:08 -0400

    deleted extra code in sport.js and show view

--- app/assets/javascripts/sport.js
+++ app/assets/javascripts/sport.js
@@ -36,7 +36,6 @@ function productInfo(data) {
 }
 
 function getDetails(id) {
-    // var description = " "
     $.ajax({
       type: "GET",
       url: "/products/" + id,
@@ -46,41 +45,3 @@ function getDetails(id) {
     }        
   });
 
-    // })
-    // $.get("/products/" + id, function(product) {
-    //   debugger
-    //   description += product
-    //   $("#product-" + id).html(description)
-    // })
-}
-
-/*
-<h1>Products</h1>
-<% @products.each do |product| %>
-  <h3><%= product.name %></h3>
-  <div id="product-<%= product.id %>"><%= truncate(product.product_description) %></div>
-  <button class="js-more" data-id="<%= product.id %>">More Info</button>
-<% end %>
-
-<script type="text/javascript" charset="utf-8">
-$(function() {
-
-  $(".js-more").on("click", function() {
-  
-  var id = $(this).data("id")
-
-    $.get("/products/" + id + ".json", function(product) {
-     
-      var inventory = "Available"
-    
-      if (product["inventory"] < 1){
-        inventory = "Sold Out"
-      }
-      var description = " " + product["description"] + " " + inventory + " "
-      
-      $("#product-" + id).html(description)
-
-    })
-  })
-})
-</script>*/
