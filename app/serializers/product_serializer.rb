7a50ce260d937840a03bf248bb0dc4282ddb0636
github username <logicae@users.noreply.github.com>
2017-05-27 18:07:16 -0400

    added relationships to serializers

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :product_name, :product_description, :user_id
+  attributes :id, :product_name, :product_description, :user_id, :sport_id
+
+  belongs_to :sport
+  belongs_to :user
 end
--- app/serializers/sport_serializer.rb
+++ app/serializers/sport_serializer.rb
@@ -1,3 +1,6 @@
 class SportSerializer < ActiveModel::Serializer
   attributes :id, :name
+
+  has_many :products
+  has_many :users, through: :products
 end
